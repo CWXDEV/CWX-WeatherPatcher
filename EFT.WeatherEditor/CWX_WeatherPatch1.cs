cdab6587d193b3d59240b76e680f5558a11ec0eb
CWX <CWXDEV@outlook.com>
2021-10-06 15:11:34 +0100

    added patch to remove global fog if their game is working correctly, they should no longer have ANY fog

--- EFT.WeatherEditor/CWX_WeatherPatch1.cs
+++ EFT.WeatherEditor/CWX_WeatherPatch1.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using Aki.Reflection.Patching;
 using Aki.Common;
 using EFT.Weather;
-using UnityEngine;
 
 namespace EFT.WeatherEditor
 {
@@ -22,26 +20,12 @@ namespace EFT.WeatherEditor
 
         private static void PostFixPatch(ref WeatherDebug __result)
         {
-            Log.Info("OLD Enabled:" + __result.Enabled.ToString());
-            Log.Info("OLD CloudDensity:" + __result.CloudDensity.ToString());
-            Log.Info("OLD Rain:" + __result.Rain.ToString());
-            Log.Info("OLD WindMagnitude:" + __result.WindMagnitude.ToString());
-            Log.Info("OLD LightningThunderProbability:" + __result.LightningThunderProbability.ToString());
-            Log.Info("OLD Fog:" + __result.Fog.ToString());
-            Log.Info("---------------------------");
             __result.Enabled = true;
             __result.CloudDensity = -1f;
             __result.Rain = 0f;
             __result.WindMagnitude = 0f;
             __result.LightningThunderProbability = 0f;
             __result.Fog = 0f;
-            Log.Info("NEW Enabled:" + __result.Enabled.ToString());
-            Log.Info("NEW CloudDensity:" + __result.CloudDensity.ToString());
-            Log.Info("NEW Rain:" + __result.Rain.ToString());
-            Log.Info("NEW WindMagnitude:" + __result.WindMagnitude.ToString());
-            Log.Info("NEW LightningThunderProbability:" + __result.LightningThunderProbability.ToString());
-            Log.Info("NEW Fog:" + __result.Fog.ToString());
-            Log.Info("-----------------------------------------------------------------");
         }
     }
 }
--- EFT.WeatherEditor/CWX_WeatherPatch3.cs
+++ EFT.WeatherEditor/CWX_WeatherPatch3.cs
@@ -1,41 +1,26 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using Aki.Reflection.Patching;
 using Aki.Common;
-using EFT.Weather;
-using UnityEngine;
-using BehaviourMachine;
 
-//namespace EFT.WeatherEditor
-//{
-//    class CWX_WeatherPatch3 : Patch
-//    {
+namespace EFT.WeatherEditor
+{
+    class CWX_WeatherPatch3 : Patch
+    {
 
-//        public CWX_WeatherPatch3() : base(T: typeof(CWX_WeatherPatch3), postfix: nameof(PostFixPatch))
-//        {
-//        }
+        public CWX_WeatherPatch3() : base(T: typeof(CWX_WeatherPatch3), postfix: nameof(PostFixPatch))
+        {
+        }
 
-//        protected override MethodBase GetTargetMethod()
-//        {
-//            return typeof(SetFog).GetMethod("Update", BindingFlags.Public | BindingFlags.Instance);
-//        }
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(CustomGlobalFog).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
 
 
-//        static void PostFixPatch(ref SetFog __result)
-//        {
-//            Aki.Common.Log.Info("OLD fog:" + __result.fog.ToString());
-//            Aki.Common.Log.Info("OLD fogMode:" + __result.fogMode.ToString());
-//            Aki.Common.Log.Info("OLD fogDensity:" + __result.fogDensity.ToString());
-//            Aki.Common.Log.Info("OLD linearFogStart:" + __result.linearFogStart.ToString());
-//            Aki.Common.Log.Info("OLD linearFogEnd:" + __result.linearFogEnd.ToString());
-//            __result.fogDensity = 0f;
-//            __result.linearFogStart = 700f;
-//            __result.linearFogEnd = 700f;
-//            Aki.Common.Log.Info("NEW fog:" + __result.fog.ToString());
-//            Aki.Common.Log.Info("NEW fogMode:" + __result.fogMode.ToString());
-//            Aki.Common.Log.Info("NEW fogDensity:" + __result.fogDensity.ToString());
-//            Aki.Common.Log.Info("NEW linearFogStart:" + __result.linearFogStart.ToString());
-//            Aki.Common.Log.Info("NEW linearFogEnd:" + __result.linearFogEnd.ToString());
-//        }
-//    }
-//}
+        private static void PostFixPatch(ref CustomGlobalFog __instance)
+        {
+            Log.Info("TURNED OFF THE GLOBAL FOG!");
+            __instance.FuncStart = 0f;
+        }
+    }
+}
