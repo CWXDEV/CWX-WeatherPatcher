9161e4a2f778680abbc605c42c3131a7555f59ef
CWX <CWXDEV@outlook.com>
2022-03-07 23:59:09 +0000

    adds details to read from config.json. *currently commented out*

---
+++ EFT.WeatherEditor/CWX_JsonConverter.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace CWX_WeatherPatch
+{
+    public class JSONClass
+    {
+        public float Cloudness { get; set; }
+        public int WindDirection { get; set; }
+        public float Wind { get; set; }
+        public float Rain { get; set; }
+        public float RainRandomness { get; set; }
+    }
+}
--- EFT.WeatherEditor/CWX_WeatherPatch1.cs
+++ EFT.WeatherEditor/CWX_WeatherPatch1.cs
@@ -1,8 +1,9 @@
-﻿using Aki.Reflection.Patching;
-using System.Reflection;
-using Newtonsoft.Json;
+using Aki.Common.Utils;
+using Aki.Reflection.Patching;
 using CWX_WeatherPatch;
+using Newtonsoft.Json;
 using System.IO;
+using System.Reflection;
 
 namespace CWX_WeatherPatcher
 {
@@ -16,19 +17,27 @@ namespace CWX_WeatherPatcher
         [PatchPostfix]
         private static void PatchPostfix(ref GClass1694 __result)
         {
-            string json = File.ReadAllText("C:/AKI PROJECT/2.2.3 build - client 16909/user/mods/CWX_WeatherPatcher 2.1.5/config.json");
-            var x = JsonConvert.DeserializeObject<JSONClass>(json.ToString());
+            //string json = File.ReadAllText("C:/AKI PROJECT/2.2.3 build - client 16909/user/mods/CWX_WeatherPatcher 2.1.5/config.json");
+            //var x = JsonConvert.DeserializeObject<JSONClass>(json.ToString());
 
-            __result.Cloudness = x.Cloudness;
-            __result.WindDirection = x.WindDirection;
-            __result.Wind = x.Wind;
-            __result.Rain = x.Rain;
-            __result.RainRandomness = x.RainRandomness;
+            //__result.Cloudness = x.Cloudness;
+            //__result.WindDirection = x.WindDirection;
+            //__result.Wind = x.Wind;
+            //__result.Rain = x.Rain;
+            //__result.RainRandomness = x.RainRandomness;
+
+            __result.Cloudness = -1;
+            __result.WindDirection = 8;
+            __result.Wind = 0;
+            __result.Rain = 0;
+            __result.RainRandomness = 0;
             __result.ScaterringFogDensity = 0;
             __result.GlobalFogDensity = 0;
             __result.GlobalFogHeight = 0;
 
-            Logger.LogInfo("MADE CHANGES TO GClass1694 SMETHOD_0!");
+
+            Log.Info("MADE CHANGES TO GClass1694 SMETHOD_0!");
+
         }
     }
 }
